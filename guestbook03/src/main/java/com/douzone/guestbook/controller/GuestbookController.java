c35d9dec87f7c8316b8ced02b2ca4a0294479942
heeyomi <heeyomi@heeyomi>
2021-06-03 14:34:13 +0900

    guestbook03 RequestMapping

---
+++ guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,55 @@
+package com.douzone.guestbook.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.guestbook.repository.GuestbookRepository;
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+	
+	@RequestMapping("")
+	public String index(Model model) {
+		List<GuestbookVo> list = guestbookRepository.find();
+		for (GuestbookVo guestbookVo : list) {
+			guestbookVo.setRegDate(guestbookVo.getRegDate().substring(0, 16));
+		}
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value="/add", method = RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		vo.setRegDate(sdf.format(new Date()));
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method = RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no",no);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method = RequestMethod.POST)
+	public String delete(@PathVariable("no") Long no, String password) {
+		GuestbookVo vo = guestbookRepository.findByNo(no);
+		if (password.equals(vo.getPassword())) {
+			guestbookRepository.delete(no, password);
+		}
+		return "redirect:/";
+	}
+	
+}
---
+++ guestbook03/src/main/java/com/douzone/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,203 @@
+package com.douzone.guestbook.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+	public boolean insert(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		
+		try {
+			conn = getConnection();
+			String sql = "insert into guestbook values(null, ?,?,?,?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+			pstmt.setString(4, vo.getRegDate());
+			
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+			
+		} catch (Exception e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+
+	public List<GuestbookVo> find() {
+		List<GuestbookVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			String sql = "select * from guestbook order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String password = rs.getString(3);
+				String message = rs.getString(4);
+				String regDate = rs.getString(5);
+				
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setPassword(password);
+				vo.setMessage(message);
+				vo.setRegDate(regDate);
+
+				result.add(vo);
+			}
+			
+		} catch (Exception e) {
+			System.out.println("error:"+e);
+		} finally {
+			try {
+				if (rs!=null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+
+	}
+	
+	public boolean delete(Long no, String pw) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		
+		try {
+			conn = getConnection();
+			String sql = "delete from guestbook where no = ? and password = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, no);
+			pstmt.setString(2, pw);
+			
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+		
+	}
+	
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.254.41:3307/webdb?characterEncoding=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패 : " + e);
+		}
+		return conn;
+	}
+
+
+	public GuestbookVo findByNo(long no) {
+		GuestbookVo result = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			String sql = "select * from guestbook where no = ? order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				Long n = rs.getLong(1);
+				String name = rs.getString(2);
+				String password = rs.getString(3);
+				String message = rs.getString(4);
+				String regDate = rs.getString(5);
+				
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(n);
+				vo.setName(name);
+				vo.setPassword(password);
+				vo.setMessage(message);
+				vo.setRegDate(regDate);
+
+				result = vo;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("error:"+e);
+		} finally {
+			try {
+				if (rs!=null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+}
