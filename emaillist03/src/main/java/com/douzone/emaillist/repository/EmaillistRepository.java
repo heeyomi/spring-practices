dddb3ad82653320368f2fbae319941d54aabd64f
heeyomi <heeyomi@heeyomi>
2021-06-03 11:58:55 +0900

    emaillist03 #finish

--- emaillist03/src/main/java/com/douzone/emaillist/repository/EmaillistRepository.java
+++ emaillist03/src/main/java/com/douzone/emaillist/repository/EmaillistRepository.java
@@ -8,9 +8,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.douzone.emaillist.vo.EmaillistVo;
 
-public class EmaillistDao {
+@Repository
+public class EmaillistRepository {
 	public Boolean insert(EmaillistVo vo) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
